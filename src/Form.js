1d5520ac930f136cbbc5757324306c9868313268
Sergi Tur Badenas <sergiturbadenas@gmail.com>
2017-06-20 15:54:13 +0200

    Added some preconfiguration to axios requests 3

--- src/Form.js
+++ src/Form.js
@@ -228,7 +228,6 @@ export default class Form {
   submit (requestType, url) {
     this.startProcessing()
     return new Promise((resolve, reject) => {
-
       this.configureAxios()
       axios[requestType](url, this.data())
         .then(response => {
